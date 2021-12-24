35da8013d9957394dd6e041a122aaa9730ba583a
Lucas-Liu <haofanglucas@gmail.com>
2021-12-24 15:53:23 +0800

    add tabs

--- src/lib/Tab.vue
+++ src/lib/Tab.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="tab-lu">
-    <p>cur</p>
+    <slot></slot>
   </div>
 </template>
 
--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="tabs-lu">
-    <component :is="defalut[0]" />
+    <div class="tabs-nav">
+      <div class="tabs-nav-items" v-for="(t, index) in titles" key="index">
+        {{ t }}
+      </div>
+    </div>
+    <div class="tabs-content">
+      <component v-for="(c, index) in defalut" key="index" :is="c" />
+    </div>
   </div>
 </template>
 
@@ -15,11 +22,38 @@ export default defineComponent({
         throw Error("only accept Tab");
       }
     });
+    const titles = defalut.map((ele) => {
+      return ele.props.title;
+    });
     return {
       defalut,
+      titles,
     };
   },
 });
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+$blue: #40a9ff;
+$color: #333;
+$border-color: #d9d9d9;
+.tabs-nav {
+  display: flex;
+  color: $color;
+  border-bottom: 1px solid $border-color;
+  &-items {
+    padding: 8px 0;
+    margin: 0 16px;
+    cursor: pointer;
+    &.selected {
+      color: $blue;
+    }
+  }
+  &:first-child {
+    margin-left: 0;
+  }
+}
+.tabs-content {
+  padding: 8px 0;
+}
+</style>
